bcf0d73b2aa1a62c868857e4ae6821a37bbadb4d
Jay <jjjjuhyuneee@naver.com>
2022-12-02 21:18:34 +0900

    1202

--- src/App.js
+++ src/App.js
@@ -2,7 +2,72 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+
+      <h1>Hello, I'm Jay.</h1>
+      <p>It's my coding archive.</p>
+      <a href="https://github.com/kim-jaay/" target="_blank">Go to Github :D</a>
+      <hr />
+      <hr />
+
+      <h2>jQuery</h2>
+      <hr />
+      <h2>01.LOTTE E&C</h2>
+      <a href="https://kim-jaay.github.io/practice/01lotteE&C/index.html" target="_blank">LOTTE E&C</a>
+      <p>2022.06.25 - 06.28</p>
+      <hr />
+
+      <h2>02.Salady</h2>
+      <a href="https://kim-jaay.github.io/practice/02salady/index.html" target="_blank">Salady</a>
+      <p>2022.07.04 - 07.08</p>
+      <hr />
+
+      <h2>03. tamburins</h2>
+      <a href="https://kim-jaay.github.io/practice/03tamburins/index.html" target="_blank">tamburins</a>
+      <p>2022.07.11 - 07.15</p>
+      <hr />
+
+      <h2>04. usm</h2>
+      <a href="https://kim-jaay.github.io/practice/04usm/index.html" target="_blank">usm</a>
+      <p>2022.07.18 - 07.29</p>
+      <hr />
+
+      <h2>05. kakao</h2>
+      <a href="https://kim-jaay.github.io/practice/05kakaofriends/index.html" target="_blank">kakao friends</a>
+      <p>2022.08.01 - 08.05</p>
+      <hr />
+
+      <h2>06. Osulloc</h2>
+      <a href="https://kim-jaay.github.io/practice/06osulloc/index.html" target="_blank">Osulloc</a>
+      <p>2022.08.08 - 08.19</p>
+      <hr />
+
+      <h2>07. H&M</h2>
+      <a href="https://kim-jaay.github.io/practice/07h&m/index.html" target="_blank">H&M</a>
+      <p>2022.08.22 - 08.26</p>
+      <hr />
+
+      <h2>08. StudioDragon</h2>
+      <a href="https://kim-jaay.github.io/practice/08studioDragon/index.html" target="_blank">Studio Dragon</a>
+      <p>2022.08.29 - 09.02</p>
+      <hr />
+      <hr />
+
+      <h2>React</h2>
+      <hr />
+      <h2>01.NASA</h2>
+      <a href="https://kim-jaay.github.io/practice_react_01/" target="_blank">NASA</a>
+      <hr />
+      <h2>02.Louvre</h2>
+      <a href="https://kim-jaay.github.io/practice_react_02/" target="_blank">Louvre</a>
+      <hr />
+      <h2>03.Sealife</h2>
+      <a href="https://kim-jaay.github.io/practice_react_03/" target="_blank">Sealife</a>
+      <hr />
+      <h2>04.COS</h2>
+      <a href="https://kim-jaay.github.io/practice_react_04_cos/" target="_blank">COS</a>
+      <hr />
+    </div>
   )
 }
 
